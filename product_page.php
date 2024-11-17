957d508828f5e094fbe3287c5210ad183d4141c7
Duc anh <nguyenanh191025@gmail.com>
2024-11-17 15:34:36 +0700

    Change div to a tag

--- product_page.php
+++ product_page.php
@@ -78,66 +78,66 @@ require_once './env.php';
         <main class="main-content">
             <h1>Nike C1TY Collection</h1>
             <div class="product-grid">
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike C1TY 'Surplus'.jpg" alt="Nike C1TY 'Surplus'">
                     <h2>Nike C1TY 'Surplus'</h2>
                     <p>1,100,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike Air Force 1 Shadow.png" alt="Nike Air Force 1 Shadow">
                     <h2>Nike Air Force 1 Shadow</h2>
                     <p>  2,350,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike Dunk Low.png" alt="Nike Dunk Low">
                     <h2>Nike Dunk Low</h2>
                     <p> 1,000,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Air Jordan 1 Low SE.jpg" alt="Air Jordan 1 Low SE">
                     <h2>Air Jordan 1 Low SE</h2>
                     <p> 2,999,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike C1TY 'Coveralls'.png" alt=" Nike C1TY 'Coveralls'">
                     <h2> Nike C1TY 'Coveralls'</h2>
                     <p>3,499,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike C1TY 'Brownstone'.jpg" alt="Nike C1TY 'Brownstone'">
                     <h2>Nike C1TY 'Brownstone'</h2>
                     <p> 3,499,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike C1TY 'Street Meat'.png" alt="Nike C1TY 'Street Meat'">
                     <h2>Nike C1TY 'Street Meat'</h2>
                     <p> 1,500,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/Nike C1TY 'Concrete'.png" alt="Nike C1TY 'Concrete'">
                     <h2>Nike C1TY 'Concrete'</h2>
                     <p> 1,050,000₫</p>
-                </div>
-                <div class="product-card" data-category="men black nike-c1ty-black">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="men black nike-c1ty-black">
                     <img src="images/anh1.png" alt="Nike C1TY Pink">
                     <h2>Nike C1TY Pink</h2>
                     <p> 2,929,000₫</p>
-                </div>
-                <div class="product-card" data-category="women blue nike-c1ty-blue">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="women blue nike-c1ty-blue">
                     <img src="images/anh2.png" alt="Nike C1TY Black">
                     <h2>Nike C1TY Black</h2>
                     <p> 2,299,000₫</p>
-                </div>
-                <div class="product-card" data-category="men brown nike-c1ty">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="men brown nike-c1ty">
                     <img src="images/Nike C1TY.png" alt="Nike C1TY ">
                     <h2>Nike C1TY </h2>
                     <p> 2,800,000₫</p>
-                </div>
-                <div class="product-card" data-category="unisex gray nike-c1ty-gray">
+                </a>
+                <a href="product_details.php" class="product-card" data-category="unisex gray nike-c1ty-gray">
                     <img src="images/anh4.png" alt="Nike C1TY Gray">
                     <h2>Nike C1TY Gray</h2>
                     <p> 3,499,000₫</p>
-                </div>
+                </a>
             </div>
         </main>
     </div>
